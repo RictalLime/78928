c1459b45ac51f22cb4b9504e8174d9d6ae4e9dfc
RictalLime <tron@hotmail.com>
2025-03-07 10:37:35 -0600

    rutas en flask01

--- flask01/app.py
+++ flask01/app.py
@@ -4,6 +4,23 @@ app = Flask(__name__)
 @app.route('/')
 def hola_mundo():
     return "hola mundo"
+
+@app.route('/adios')
+def adios_mundo():
+    return "adios mundo"
+
+@app.route('/hola')
+def hola_html():
+    return "<h1 style=color:red>hola html</h1>"
+
+@app.route('/json')
+def algo():
+    return '{"nombre":"john"}'
+
+@app.route('/xml')
+def xml():
+    return '<nombre>john</nombre>'
+
 if __name__== "__main__":
     app.run(host='0.0.0.0',
 debug=True)
