6b28e1791184f1b6154efdb358efa41d7587f8e0
RictalLime <tron@hotmail.com>
2025-03-21 10:54:01 -0600

    flask03

---
+++ flask03/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from rutas import rutas_bp
+
+app = Flask (__name__)
+
+app.register_blueprint(rutas_bp)
+
+@app.route('/')
+def inicio ():
+    return 'pagina de inicio'
+
+if __name__ == '_main_':
+    app.run(host='0.0.0.0', debug=True)
